0da9768ec7e9b116e4bcd252323776f54be67bcd
Jo Owre <jfowre@gmail.com>
2014-09-09 14:34:26 -0400

    uses fix for setting cookie correctly

--- lib/action_dispatch/middleware/session/redis_store.rb
+++ lib/action_dispatch/middleware/session/redis_store.rb
@@ -7,10 +7,19 @@ module ActionDispatch
     class RedisStore < Rack::Session::Redis
       include Compatibility
       include StaleSessionCheck
+      include SessionObject
+
       def initialize(app, options = {})
         options = options.dup
         options[:redis_server] ||= options[:servers]
         super
+      end
+
+      private
+
+      def set_cookie(env, session_id, cookie)
+        request = ActionDispatch::Request.new(env)
+        request.cookie_jar[key] = cookie
       end
     end
   end
