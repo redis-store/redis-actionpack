f234ba7d3f3ec446b8b1846e0d9fccc1cf94815f
Ryan Bigg <git@ryanbigg.com>
2015-09-11 08:53:57 +1000

    These silly version tests

--- test/redis/actionpack/version_test.rb
+++ test/redis/actionpack/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Redis::ActionPack::VERSION do
   it 'returns current version' do
-    Redis::ActionPack::VERSION.must_equal '4.0.0'
+    Redis::ActionPack::VERSION.must_equal '4.0.1'
   end
 end
