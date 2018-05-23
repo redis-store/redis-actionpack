9dadf90910718f26d26937a4443c6597fd3e72c5
Tom Scott <tscott@weblinc.com>
2018-05-23 10:13:13 -0400

    Support Signed and Encrypted Cookie Storage
    With the `:signed` option passed into the Redis session store, you can
    now ensure that sessions will be set up in the browser using a
    signed/encrypted cookie. This prevents user tampering by changing their
    session ID or the data within the cookie.
    
    Closes #21

--- lib/action_dispatch/middleware/session/redis_store.rb
+++ lib/action_dispatch/middleware/session/redis_store.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'redis-store'
 require 'redis-rack'
 require 'action_dispatch/middleware/session/abstract_store'
 
 module ActionDispatch
   module Session
+    # Session storage in Redis, using +Redis::Rack+ as a basis.
     class RedisStore < Rack::Session::Redis
       include Compatibility
       include StaleSessionCheck
@@ -17,17 +20,30 @@ module ActionDispatch
 
       private
 
-      def set_cookie(env, session_id, cookie)
-        if env.is_a? ActionDispatch::Request
-          request = env
-        else
-          request = ActionDispatch::Request.new(env)
-        end
-        request.cookie_jar[key] = cookie.merge(cookie_options)
+      def set_cookie(env, _session_id, cookie)
+        request = wrap_in_request(env)
+        cookie_jar(request)[key] = cookie.merge(cookie_options)
+      end
+
+      def get_cookie(request)
+        cookie_jar(request)[key]
+      end
+
+      def wrap_in_request(env)
+        return env if env.is_a?(ActionDispatch::Request)
+        ActionDispatch::Request.new(env)
       end
 
       def cookie_options
         @default_options.slice(:httponly, :secure)
+      end
+
+      def cookie_jar(request)
+        if @default_options[:signed]
+          request.cookie_jar.signed_or_encrypted
+        else
+          request.cookie_jar
+        end
       end
     end
   end
